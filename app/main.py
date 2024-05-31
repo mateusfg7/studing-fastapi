668161d6555a14d0d76c6abb223e4c88ac2d2334
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-05-31 19:49:19 +0000

    feat(routes): add new routes
    * List todos `GET /list`
    * Add todo `POST /add`
    * Toggle completed `PUT /toggle-completed/{todo_id}`

--- app/main.py
+++ app/main.py
@@ -2,31 +2,63 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from datetime import datetime
+
+from app.database import todos, Todo
 
 app = FastAPI()
 
-@app.get('/')
-def root():
-    return {"message": "Hello World!"}
+def filter_todos(todo: Todo, completed: bool):
+    return todo['completed'] == completed
 
+@app.get('/list')
+def list_todos(completed: Union[bool, None] = None):
+    if completed == None:
+        return todos
+    else:
+        return list(filter(
+            lambda todo: filter_todos(todo, completed),
+            todos
+        ))
 
-@app.get('/items/{item_id}')
-def read_item(item_id: int, sort: Union[str, None] = 'Asc', limit: Union[int, None] = 5):
-    return {
-        "item_id": item_id,
-        "sort": sort,
-        "limit": limit
-    }
+def id_is_duplicated(todos: list[Todo], id: int):
+    if any(todo['id'] == id for todo in todos):
+        return True
+    else:
+        return False
+
+class NewTodo(BaseModel):
+    id: int
+    title: str
     
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-@app.put("/items/{item_id}")
-def update_items(item_id: int, item: Item):
-    return {
-        "item_name": item.name,
-        "item_id": item_id
-    }
+@app.post('/add')
+def create_todo(new_todo: NewTodo):
+    if id_is_duplicated(todos, new_todo.id):
+        return {'error': f'There is already a todo with ID = {new_todo.id}'}
+    else:
+        todo: Todo = {
+            'id': new_todo.id,
+            'title': new_todo.title,
+            'completed': False,
+            'created_at': datetime.now(),
+            'updated_at': datetime.now()
+        }
+        todos.append(todo)
+        return {'message': 'Todo created successfully!', 'todo': todo}
+    
+@app.put('/toggle-completed/{todo_id}')
+def complete_todo(todo_id: int):
+    global todos
+    
+    updated_todos: list[Todo] = []
+    completed = None
+    for todo in todos:
+        if todo['id'] == todo_id:
+            updated_todos.append({ **todo, 'completed': not todo['completed'] })
+            completed = not todo['completed']
+        else:
+            updated_todos.append(todo)
+    
+    todos = updated_todos.copy()
+    return {'message': f'Todo {todo_id} marked as {'Completed' if completed else 'Uncompleted'}'}
+        
