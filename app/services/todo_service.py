bd1cb9fe0af0705207eebc490eb353eef65d9729
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-06-01 18:00:33 +0000

    fix(todo_service): update todo "updated_at" timestamp when toggle completed

--- app/services/todo_service.py
+++ app/services/todo_service.py
@@ -4,6 +4,7 @@ from typing import Union
 from models.db import Todo
 from database import todos
 
+
 def add(id: int, title: str) -> Todo:
     todo: Todo = {
         'id': id,
@@ -13,20 +14,22 @@ def add(id: int, title: str) -> Todo:
         'updated_at': datetime.now()
     }
     todos.append(todo)
-    
+
     return todo
+
 
 def get_by_id(id: int) -> Todo | None:
     founded_todos: list[Todo] = []
-    
+
     for todo in todos:
         if todo['id'] == id:
             founded_todos.append(todo)
-    
+
     if len(founded_todos) == 0:
         return None
     else:
         return founded_todos[0]
+
 
 def toggle_completed(todo_id: int):
     global todos
@@ -35,7 +38,8 @@ def toggle_completed(todo_id: int):
     completed = None
     for todo in todos:
         if todo['id'] == todo_id:
-            updated_todos.append({**todo, 'completed': not todo['completed']})
+            updated_todos.append(
+                {**todo, 'completed': not todo['completed'], 'updated_at': datetime.now()})
             completed = not todo['completed']
         else:
             updated_todos.append(todo)
@@ -43,8 +47,10 @@ def toggle_completed(todo_id: int):
     todos = updated_todos.copy()
     return completed
 
+
 def filter_todos(todo: Todo, completed: bool):
     return todo['completed'] == completed
+
 
 def list_all(completed: Union[bool, None] = None) -> list[Todo]:
     if completed == None:
