38a69cb25e9082f4283b58ffd0b43e4226a29f2b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-06-01 14:16:21 +0000

    refactor(services): create todo service

--- app/routes/add_todo.py
+++ app/routes/add_todo.py
@@ -1,34 +1,17 @@
 from fastapi import APIRouter
 
-from datetime import datetime
-
-from database import todos
-
-from models.db import Todo
 from models.dto import NewTodo
+
+from services import todo_service
 
 
 router = APIRouter()
 
 
-def id_is_duplicated(todos: list[Todo], id: int):
-    if any(todo['id'] == id for todo in todos):
-        return True
-    else:
-        return False
-
-
 @router.post('/add')
-def create_todo(new_todo: NewTodo):
-    if id_is_duplicated(todos, new_todo.id):
+def add_todo(new_todo: NewTodo):
+    if todo_service.get_by_id(new_todo.id) != None:
         return {'error': f'There is already a todo with ID = {new_todo.id}'}
     else:
-        todo: Todo = {
-            'id': new_todo.id,
-            'title': new_todo.title,
-            'completed': False,
-            'created_at': datetime.now(),
-            'updated_at': datetime.now()
-        }
-        todos.append(todo)
+        todo = todo_service.add(id=new_todo.id, title=new_todo.title)
         return {'message': 'Todo created successfully!', 'todo': todo}
